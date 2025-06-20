846ace6807adadbe219f2fa252938582f6eddf7e
Tuan Nguyen Vinh <vinhtuanbm@gmai.com>
2025-06-20 22:44:57 +0700

    fix: standardize header glow effect and padding across About, Contact, and Project pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Project() {
   return (
     <div className="container w-full mx-auto flex flex-col items-center px-2 sm:px:5 md:px-10 lg:px-20">
-      <div className="p-20">
+      <div className="py-20">
         <h1 className="relative text-3xl md:text-4xl lg:text-5xl font-[1000] text-center">
           About Me
-          <div className="absolute -z-1 top-[80%] w-[110%] h-[110%] left-1/2 transform -translate-x-1/2">
-            <Image src='/images//svg/page-header-glow.svg' width={290} height={85} alt="Header glow" />
+          <div className="absolute -z-1 top-[80%] w-[140%] h-[140%] left-1/2 transform -translate-x-1/2">
+            <Image className="object-cover" src='/images/svg/page-header-glow.svg' fill={true} alt="Header glow" />
           </div>
         </h1>
       </div>
